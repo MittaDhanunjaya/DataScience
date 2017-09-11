c7657978d51a600cf7e325db51b1f883bd59517d
Dhanunjaya Mitta <mitta.dhanunjaya@gmail.com>
2017-09-11 22:04:19 +0200

    Create first.R
    #This is all what i learned today in R Programming language

---
+++ first.R
@@ -0,0 +1,4 @@
+weights <- c(52,58,63.5,68,73,71,78,75,73.5,74.5)
+months <- c("January","February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December")
+names(weights) <- months
+mean(weights)
