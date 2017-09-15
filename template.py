00630d5d7ec7cea77439536391e2e1feea38c43a
Dhanunjaya Mitta <mitta.dhanunjaya@gmail.com>
2017-09-15 22:10:24 +0200

    Update template.py

--- template.py
+++ template.py
@@ -9,3 +9,9 @@ dataset = pd.read_csv('Data.csv')
 #Here we need to take two matrices one for non-dependent variables and one for dependent variables.
 X = dataset.ilog[: , :-1].values #This will select all rows and columns except that last column
 Y = dataset.ilog[: , 3].values #This will select all rows and the last column.
+# Take care of missing elements
+from sklearn.preprocessing import Imputer 
+imputer =Imputer(missing_values='NaN', strategy='mean', axis=0, verbose=0, copy=True)
+#axis=0 for column wise mean and 1 for row wise mean
+imputer=imputer.fit(X[:, 1:3])
+X[:, 1:3]= imputer.transform(X[:,1:3])
