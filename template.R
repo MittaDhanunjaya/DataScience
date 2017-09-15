8a9d42e6b063af886bccb06e5b7f8c9b703eceb8
Dhanunjaya Mitta <mitta.dhanunjaya@gmail.com>
2017-09-15 20:39:47 +0200

    Create template.R

---
+++ template.R
@@ -0,0 +1,7 @@
+#Here no need to import libraries in R as we do in python. 
+#R will automatically includes all the necessary packages that are previously in-built with the application.
+#select working directory.
+#On the right hand select files and select the folder in which .csv file is present. and go to more and select set as working directory.
+#include .csv file
+dataset = read.csv('Data.csv') #Run this line 
+#press View(dataset) in the console to confirm the .csv file in the program
