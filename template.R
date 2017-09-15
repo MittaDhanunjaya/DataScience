da30d4b40769d333b75fa104e47c82c3d7cfced9
Dhanunjaya Mitta <mitta.dhanunjaya@gmail.com>
2017-09-15 22:12:51 +0200

    Update template.R

--- template.R
+++ template.R
@@ -5,3 +5,9 @@
 #include .csv file
 dataset = read.csv('Data.csv') #Run this line 
 #press View(dataset) in the console to confirm the .csv file in the program
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN= function(x) mean(x, na.rm= TRUE)),
+                     dataset$Age)
+dataset$Salary= ifelse(is.na(dataset$Salary),
+                       ave(dataset$Salary,FUN= function(y) mean(y,na.rm=TRUE)),
+                       dataset$Salary)
