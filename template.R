21bbaea8e06d88ac68569a08b94f1f468e88b63e
Dhanunjaya Mitta <mitta.dhanunjaya@gmail.com>
2017-09-16 15:18:43 +0200

    Update template.R

--- template.R
+++ template.R
@@ -27,3 +27,8 @@ set.seed(123)
 split=sample.split(dataset$Purchased, SplitRatio= 0.8)
 training_set=subset(dataset,split==TRUE)
 testing_set=subset(dataset,split==FALSE)                       
+#feature scaling
+#we have to scale the elements since if we compare two elements those two should be in some less distance if u take sqrt(1324131^2-2^2)
+                       #it would be very dominating.
+training_set[,2:3]=scale(training_set[,2:3])
+testing_set[,2:3]=scale(testing_set[,2:3])
